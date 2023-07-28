2274dd8a4c3ea8f44e1216b1ed90223dc715acc9
Eric Santana <efms.king@gmail.com>
2023-07-28 20:14:10 -0300

    UPDATE: camera initial layout

--- src/Screens/QrScanner.js
+++ src/Screens/QrScanner.js
@@ -1,9 +1,11 @@
-import { Text, Icon, Container, HStack, Button, Box } from 'native-base';
-import { StyleSheet } from 'react-native';
+import { Text, Icon, Container, Button, Box, VStack, HStack, IconButton } from 'native-base';
+import { StyleSheet, Dimensions } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 import { useNavigation } from '@react-navigation/native';
+import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
 
@@ -27,8 +29,10 @@ export default function QrScanner() {
   }, []);
 
   useEffect(() => {
-    if (devices) console.log(devices);
-  }, [devices]);
+    if (barcodes.length) {
+      console.log(barcodes, '--barcodes');
+    }
+  }, [barcodes]);
 
   if (device == null) return <Text>Loading</Text>;
   return (
@@ -36,22 +40,58 @@ export default function QrScanner() {
       <Camera
         device={device}
         isActive={true}
+        torch={flash ? 'on' : 'off'}
         style={StyleSheet.absoluteFill}
         frameProcessor={frameProcessor}
         frameProcessorFps={5}
       />
-      {barcodes.map((barcode, idx) => (
-        <Text key={idx} style={styles.barcodeTextURL}>
-          {barcode.displayValue}
-        </Text>
-      ))}
+      <VStack p="10px">
+        <Box bg="white.400" w="100%" maxH="60px">
+          <HStack justifyContent={'space-between'}>
+            <IconButton
+              icon={<Icon as={IonIcons} name="chevron-back" />}
+              size="md"
+              borderRadius="full"
+              bg="coolGray.800:alpha.20"
+              onPress={() => {
+                navigation.goBack();
+              }}
+              _icon={{
+                color: 'coolGray.300',
+                size: 'lg',
+              }}
+              _pressed={{
+                bg: 'coolGray.800:alpha.40',
+              }}
+            />
+            <IconButton
+              icon={<Icon as={IonIcons} name={flash ? 'flash' : 'flash-off'} />}
+              size="md"
+              borderRadius="full"
+              bg={flash ? 'primary.600' : 'coolGray.800:alpha.20'}
+              onPress={() => setFlash(!flash)}
+              _icon={{
+                color: 'coolGray.300',
+                size: 'lg',
+              }}
+              _pressed={{
+                bg: 'coolGray.800:alpha.40',
+              }}
+            />
+          </HStack>
+        </Box>
+        <Box w="100%" h={Dimensions.get('window').height * 0.65 - 60}></Box>
+        <Box
+          w="100%"
+          h={Dimensions.get('window').height * 0.35}
+          display="flex"
+          justifyContent={'flex-end'}
+        >
+          <Box rounded="8" shadow={2} bg="coolGray.100" p="15px" m="10px" mb="25px">
+            <Text>Testando para ver se ta tudo certinho </Text>
+          </Box>
+        </Box>
+      </VStack>
     </Box>
   );
 }
-const styles = StyleSheet.create({
-  barcodeTextURL: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-});
